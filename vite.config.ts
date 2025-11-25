de111bb375c5604f6f1f7bf800bda5fea33c20ef
raenmedia <raenmedia@gmail.com>
2025-11-25 21:34:54 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      optimizeDeps: {
+        include: ['react-router-dom']
       }
     };
 });
